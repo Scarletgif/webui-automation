58fda03e348e5178251993e80c12fbc383d9dd2f
= <=>
2020-05-11 18:17:17 +0700

    commint 11May_0.2

--- src/test/java/vn/amabuy/pages/HomePage.java
+++ src/test/java/vn/amabuy/pages/HomePage.java
@@ -1,6 +1,15 @@
 package vn.amabuy.pages;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class HomePage extends PageObject {
+
+    @FindBy(xpath = "//*[@id=\"nav-link-accountList\"]")
+    WebElementFacade loginLink ;
+
+    public void clickonLoginLink(){
+        loginLink.click();
+    }
 }
