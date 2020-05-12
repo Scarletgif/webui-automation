28d03d5152107d7967f9b6ae41b59439eb6ec841
= <=>
2020-05-12 11:06:21 +0700

    Commit 12May

---
+++ src/test/java/vn/amabuy/features/browser/WhenOpeningTheWeb.java
@@ -0,0 +1,25 @@
+package vn.amabuy.features.browser;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import vn.amabuy.steps.serenity.HomeSteps;
+
+@RunWith(SerenityRunner.class)
+public class WhenOpeningTheWeb {
+
+    @Managed
+    WebDriver driver;
+
+    @Steps
+    HomeSteps scarletGif;
+
+    @Test
+    public void create_account(){
+        scarletGif.create_account("Minh Minh", "minh@gmail.com","12345689","12345689");
+    }
+
+}
--- src/test/java/vn/amabuy/pages/RegisterPage.java
+++ src/test/java/vn/amabuy/pages/RegisterPage.java
@@ -5,24 +5,32 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 
 public class RegisterPage extends PageObject {
-    private static final String INPUT_TYPE_EMAIL="//*[@id=\"ap_email\"]";
-    private static final String INPUT_TYPE_PASSWORD="//*[@id=\"ap_password\"]";
-    private static final String INPUT_TYPE_CHECKPASSWORD="//*[@id=\"ap_password_check\"]";
-    private static final String LABEL_PASSWORD="//*[@id=\"ap_register_form\"]/div/div/div[3]/div[1]/label/following-sibling::input";
-    @FindBy(xpath = "//*[@id=\"ap_customer_name\"]")
-    WebElementFacade yourNamefield;
+    private static final String YOUR_NAME_FIELD = "Your name";
+    private static final String EMAIL_FIELD="Email";
+    private static final String PASSWORD_FIELD="Password";
+    private static final String CONFIRM_PASS_FIELD="Password";
+
+
     public void enterTextIntoYourName(String yourName){
-        yourNamefield.type(yourName);
+        element(Target.of(YOUR_NAME_FIELD)).type(yourName);
     }
 
     public void enterTextIntoEmail (String email){
-        $(INPUT_TYPE_EMAIL).type(email);
+        element(Target.of(EMAIL_FIELD)).type(email);
     }
 
     public void enterTextIntoPassword(String password){
-        element(LABEL_PASSWORD).type(password);
+
+        element(Target.of(PASSWORD_FIELD)).type(password);
     }
     public void enterTextIntoCheckPassword(String password){
-        $(INPUT_TYPE_CHECKPASSWORD).type(password);
+        element(Target.of(CONFIRM_PASS_FIELD)).type(password);
+    }
+
+    static class Target{
+        private static final String TARGET = "//label[contains(text(),'%s')]/following-sibling::input";
+        public static String of (String label){
+            return String.format(TARGET,label);
+        }
     }
 }
