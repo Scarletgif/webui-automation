bbfcbba59c389da4b1450f395873f2cfed6fff92
= <=>
2020-05-20 09:31:48 +0700

    Data Driven Test

--- src/test/java/vn/amabuy/features/register/WhenRegisterNewAccount.java
+++ src/test/java/vn/amabuy/features/register/WhenRegisterNewAccount.java
@@ -4,6 +4,7 @@ import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.junit.annotations.Qualifier;
+import net.thucydides.junit.annotations.TestData;
 import net.thucydides.junit.annotations.UseTestDataFrom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +13,11 @@ import vn.amabuy.features.models.Account;
 import vn.amabuy.steps.serenity.HomeSteps;
 import vn.amabuy.steps.serenity.LoginSteps;
 import vn.amabuy.steps.serenity.RegisterSteps;
-//test git
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @RunWith(SerenityParameterizedRunner.class)
-@UseTestDataFrom("src/test/resources/registernewaccount_testdata1.csv,src/test/resources/registernewaccount_testdata2.csv")
 public class WhenRegisterNewAccount {
     @Managed
     WebDriver driver;
@@ -25,26 +27,40 @@ public class WhenRegisterNewAccount {
     LoginSteps loginSteps;
     @Steps
     RegisterSteps registerSteps;
+    @TestData
+    public static Collection<Object[]> testData(){
+        return Arrays.asList(new Object[] []
+                {
+                        {"Minh Minh","minh@gmail.com","12345689","123456"},
+                        {"MaiDao","mai@gmail.com","012345","0123"}
+                }
+        );
+    }
+
 
     //Data driven test
     private String fullName;
     private String email;
     private String password;
     private String rePassword;
+    public WhenRegisterNewAccount (String fullName, String email, String password,String rePassword){
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+        this.rePassword=rePassword;
+    }
 
-    //Account accountInfo = new Account("Minh Minh","minh@gmail.com","123456789","123456");
     Account accountInfo;
-
     String errMsg ="Passwords must match";
     @Test
     public void register_new_account_with_incorrect_password(){
-
         accountInfo=new Account (fullName,email,password,rePassword);
         homeSteps.visit_application();
         homeSteps.click_on_login_link();
         loginSteps.click_on_register_link();
         registerSteps.register_new_account(accountInfo);
         registerSteps.should_see_warning_error_message(errMsg);
+        //System.out.println(fullName);
     }
 
 
