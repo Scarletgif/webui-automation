dc61176a86bd5ee5982e6a8e10d4658370b70a30
= <=>
2020-05-26 17:24:15 +0700

    Commit lesson-36

--- src/test/java/vn/amabuy/pages/AlertPage.java
+++ src/test/java/vn/amabuy/pages/AlertPage.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import vn.amabuy.MyPageObject;
 
-public class AlertPage extends PageObject {
+public class AlertPage extends MyPageObject {
 
 
     public void answerPrompt(String answer) {
